113a7e3d64c8d0dc218e18df803b048e6f634f9d
Darth Maure <darth.maure@hotmail.com>
2020-12-02 11:04:11 +0100

    Initial commit

---
+++ src/PathDesigner/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace PathDesigner
+{
+    public class MainViewModel : BaseItem
+    {
+        public MainViewModel()
+        {
+            AddNewItemCommand = new RelayCommand(x => true,
+                x =>
+                {
+                    var newItem = new PathDesignerItem { Name = "new item", Stroke = "1.0", Heigth = "80", Width = "80" };
+                    Items.Add(newItem);
+                    SelectedItem = newItem;
+                });
+        }
+
+
+        private ObservableCollection<PathDesignerItem> _items = new ObservableCollection<PathDesignerItem>();
+
+        public ObservableCollection<PathDesignerItem> Items
+        {
+            get => _items;
+            set => SetProperty(ref _items, value);
+        }
+
+        private PathDesignerItem _selectedItem;
+
+        public PathDesignerItem SelectedItem
+        {
+            get => _selectedItem;
+            set => SetProperty(ref _selectedItem, value);
+        }
+        
+        
+        public ICommand AddNewItemCommand { get; set; }
+    }
+}
---
+++ src/PathDesigner/PathDesignerItem.cs
@@ -0,0 +1,52 @@
+﻿namespace PathDesigner
+{
+    public class PathDesignerItem : BaseItem
+    {
+        private string _data;
+        public string Data
+        {
+            get => _data;
+            set => SetProperty(ref _data, value);
+        }
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+
+        private string _stroke;
+        public string Stroke
+        {
+            get => _stroke;
+            set
+            {
+                if (double.TryParse(value, out var x))
+                    SetProperty(ref _stroke, value);
+            }
+        }
+
+        private string _heigth;
+        public string Heigth
+        {
+            get => _heigth;
+            set
+            {
+                if (double.TryParse(value, out var x))
+                    SetProperty(ref _heigth, value);
+            }
+        }
+
+        private string _width;
+        public string Width
+        {
+            get => _width;
+            set
+            {
+                if (double.TryParse(value, out var x))
+                    SetProperty(ref _width, value);
+            }
+        }
+    }
+}
